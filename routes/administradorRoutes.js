bbca0c991b7f03e69355cc7cac30ace876e86139
Iktan Strategies <antonio.sanchez@iktanst.com>
2022-08-13 15:41:56 -0500

    first commit

---
+++ routes/administradorRoutes.js
@@ -0,0 +1,30 @@
+import express from "express";
+const router = express.Router();
+import { 
+    registrar, 
+    perfil, 
+    confirmar,
+    autenticar,
+    olvidePassword,
+    comprobarToken,
+    nuevoPassword,
+    actualizarPerfil,
+    actualizarPassword,
+    obtenerAdministradores
+} from '../controllers/administradorController.js'
+import checkAuth from '../middleware/authMiddleware.js'
+
+//* Rutas para el area publica
+router.post('/', registrar);
+router.get('/confirmar/:token', confirmar);
+router.post('/login', autenticar);
+router.post('/olvide-password', olvidePassword);
+router.route('/olvide-password/:token').get(comprobarToken).post(nuevoPassword);
+
+//* Rutas privadas
+router.get('/perfil',checkAuth, perfil);
+router.put('/perfil/:id', checkAuth, actualizarPerfil)
+router.put('/actualizar-password', checkAuth, actualizarPassword)
+router.get('/obtener-administradores', obtenerAdministradores)
+
+export default router;
